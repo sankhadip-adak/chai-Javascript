7767004a0c93b9cbcb65b0b8e3eb4190f45ee2fc
sankhadip-adak <sankhadipadak19@gmail.com>
2024-01-15 22:55:56 +0530

    chai & javascript

---
+++ DataType Conversion/script.js
@@ -0,0 +1,11 @@
+
+let score = 33
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+ 
+
+let isLogIn = 6
+console.log(typeof (isLogIn));
+let booleanIsLogin = Boolean(isLogIn)
+console.log(booleanIsLogin);
---
+++ let,Var,const/script.js
@@ -0,0 +1,8 @@
+let FullName = "sankhadip"
+var age = "24"
+const experience = "2"
+city = "kolkata"
+age = "30"
+FullName = "Adak"
+
+console.table([FullName, age, experience,city]);
