7de376fdf7c254168c06267ed850b1423b8d7f9a
sankhadip-adak <sankhadipadak19@gmail.com>
2024-02-05 23:40:26 +0530

    Math Basics complete

--- 3.number & maths/script.js
+++ 3.number & maths/script.js
@@ -1,16 +1,34 @@
 // ***************Number***************
 
 // let score = Number(400);
-// console.log(score.toFixed(2));
+// console.log(score.toFixed(2));//400.00
 // console.log(score.toString());
 // console.log(typeof(score));
 // console.log(typeof (score));
 // console.log(score.toFixed(2));
 
 // let newNumber = 400.235;
-// console.log(newNumber.toPrecision(4));
+// console.log(newNumber.toPrecision(4)); //400.2
 
 // let num = 400000000;
-// console.log(num.toLocaleString('en-IN'));
+// console.log(num.toLocaleString('en-IN'));//40,000,000
 
 // ***************Maths***************
+ 
+// console.log(Math); //object
+// console.log(Math.abs(-4)) //4
+// console.log(Math.round(4.6));// 5
+// console.log(Math.floor(4.8)); //4
+// console.log(Math.ceil(4.8));//5
+// console.log(Math.max(4,8,5,10)); //10
+// console.log(Math.min(4,8,5,10)); //4
+// console.log(Math.random()); //0 To 1
+// console.log((Math.random()*10) + 1);
+
+let max = 20;
+let min = 10;
+ console.log(Math.floor(Math.random() * (max - min)) + min)
+
+
+
+
